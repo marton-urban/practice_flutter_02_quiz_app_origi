4c9a9175f37e00ab33ed27ce926e62891a9c423e
Marton Urban <urban.marton@gmail.com>
2023-02-13 00:12:24 +0100

    first commit

---
+++ lib/answer.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class Answer extends StatelessWidget {
+  final void Function() selectHandler;
+  final String answerText;
+
+  const Answer(this.selectHandler, this.answerText, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: double.infinity,
+      child: ElevatedButton(
+        onPressed: selectHandler,
+        child: Text(answerText),
+      ),
+    );
+  }
+}
